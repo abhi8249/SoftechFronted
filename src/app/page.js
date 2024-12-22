c884e2be43bf4478dcdb5a2515ffbb1a7a46fde2
Abinash Dash <abinash.dash@hyscaler.com>
2024-12-22 14:56:04 +0530

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,5 @@
 "use client";
 import { useState } from "react";
-import Link from "next/link";
 import NavBar from "../components/NavBar";
 import Slider from "@/components/Slider";
 import About from "@/components/About";
